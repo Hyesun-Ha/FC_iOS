8f935d14b86aad39057e260d6cfe0ffa2d7de636
Hahyesun <hahye3@naver.com>
2020-08-31 20:11:51 +0900

    Added Storyboard - ToDoListVC related UI

---
+++ TodoList/TodoList/Tasks/TodoListCell.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListCell.swift
+//  TodoList
+//
+//  Created by 하혜선 on 2020/08/31.
+//  Copyright © 2020 하혜선. All rights reserved.
+//
+
+import UIKit
+
+class TodoListCell: UICollectionViewCell {
+    @IBOutlet weak var checkButton: UIButton!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var deleteButton: UIButton!
+    @IBOutlet weak var strikeThroughView: UIView!
+    
+}
---
+++ TodoList/TodoList/Tasks/TodoListHeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListHeaderView.swift
+//  TodoList
+//
+//  Created by 하혜선 on 2020/08/31.
+//  Copyright © 2020 하혜선. All rights reserved.
+//
+
+import UIKit
+
+class TodoListHeaderView: UICollectionReusableView {
+    @IBOutlet weak var sectionTitleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
